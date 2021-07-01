16c5ecbbbfe86612877212a365e1091eed015914
Harry Greentree <harrygreentree@icloud.com>
2021-07-01 23:09:46 +0100

    Fixed a bug where chicken entities were not rendering as intended

--- src/main/java/io/github/TheBlackSquidward/resourcechickens/client/render/CustomChickenRenderer.java
+++ src/main/java/io/github/TheBlackSquidward/resourcechickens/client/render/CustomChickenRenderer.java
@@ -5,18 +5,30 @@ import io.github.TheBlackSquidward.resourcechickens.api.ChickenRegistry;
 import io.github.TheBlackSquidward.resourcechickens.api.ChickenRegistryObject;
 import io.github.TheBlackSquidward.resourcechickens.client.model.CustomChickenModel;
 import io.github.TheBlackSquidward.resourcechickens.entities.CustomChickenEntity;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.util.ResourceLocation;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 public class CustomChickenRenderer extends MobRenderer<CustomChickenEntity, CustomChickenModel<CustomChickenEntity>> {
     public CustomChickenRenderer(EntityRendererManager rendererManager) {
         super(rendererManager, new CustomChickenModel<>(), 0.3f);
     }
 
+    @ParametersAreNonnullByDefault
+    @Nullable
+    @Override
+    protected RenderType getRenderType(CustomChickenEntity customChickenEntity, boolean p_230496_2_, boolean p_230496_3_, boolean p_230496_4_) {
+        return RenderType.entityTranslucent(this.getTextureLocation(customChickenEntity));
+    }
+
+    @ParametersAreNonnullByDefault
     @Override
     public ResourceLocation getTextureLocation(CustomChickenEntity entity) {
         ChickenRegistryObject chickenRegistryObject = ChickenRegistry.getChickenRegistryObjectbyEntity(entity);
-        return new ResourceLocation(ResourceChickens.MODID, "textures/entity/" + chickenRegistryObject.getEntityName() + ".png");
+        return new ResourceLocation(ResourceChickens.MOD_ID, "textures/entity/" + chickenRegistryObject.getEntityName() + ".png");
     }
 }
