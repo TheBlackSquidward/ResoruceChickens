ca11656303d1e2cbad0adb9a4e35751cd8f54dd6
Harry Greentree <harrygreentree@icloud.com>
2021-07-07 10:16:16 +0100

    Finally fixed the RoostTooltipCallback.java

--- src/main/java/io/github/TheBlackSquidward/resourcechickens/compat/jei/RoostTooltipCallback.java
+++ src/main/java/io/github/TheBlackSquidward/resourcechickens/compat/jei/RoostTooltipCallback.java
@@ -1,11 +1,18 @@
 package io.github.TheBlackSquidward.resourcechickens.compat.jei;
 
-import io.github.TheBlackSquidward.resourcechickens.recipes.recipe.RoostRecipe;
+import io.github.TheBlackSquidward.resourcechickens.api.data.RoostData;
+import io.github.TheBlackSquidward.resourcechickens.recipes.RoostRecipe;
 import mezz.jei.api.gui.ingredient.ITooltipCallback;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.StringTextComponent;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.text.DecimalFormat;
 import java.util.List;
 
-public class RoostTooltipCallback implements ITooltipCallback {
+public class RoostTooltipCallback implements ITooltipCallback<ItemStack> {
+
+    private static final DecimalFormat FORMATTER = new DecimalFormat("0.0%");
 
     private final RoostRecipe roostRecipe;
 
@@ -13,9 +20,18 @@ public class RoostTooltipCallback implements ITooltipCallback {
         this.roostRecipe = roostRecipe;
     }
 
+    @ParametersAreNonnullByDefault
     @Override
-    public void onTooltip(int slotIndex, boolean input, Object ingredient, List tooltip) {
-        //TODO
+    public void onTooltip(int slotIndex, boolean input, ItemStack ingredient, List tooltip) {
+        if(!input) {
+            RoostData roostData = roostRecipe.getCustomChickenData().getRoostData();
+            roostData.getRoostOutputs().forEach((roostOutputData) -> {
+                if(ingredient.sameItem(roostOutputData.getItemOutputData().getDisplayItem())) {
+                    tooltip.add(new StringTextComponent("Minimum Drop Amount: " + roostOutputData.getItemOutputData().getMinAmount()));
+                    tooltip.add(new StringTextComponent("Maximum Drop Amount: " + roostOutputData.getItemOutputData().getMaxAmount()));
+                    tooltip.add(new StringTextComponent("Chance: " + FORMATTER.format(roostOutputData.getChance())));
+                }
+            });
+        }
     }
-
 }
